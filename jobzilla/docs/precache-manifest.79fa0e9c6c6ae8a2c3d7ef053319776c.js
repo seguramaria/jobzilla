1ae238de829efb4be2a984bf0a0e593ffa794892
MariaesSegura <msegber@gmail.com>
2020-09-25 20:00:01 +0200

    Create react app

---
+++ jobzilla/docs/precache-manifest.79fa0e9c6c6ae8a2c3d7ef053319776c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751ee3905c6d2f1960d9432f636d318f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f6363c179028512aa04",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "620a9680f568ac03ae9f",
+    "url": "./static/js/2.edff1539.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.edff1539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6363c179028512aa04",
+    "url": "./static/js/main.03ddcae4.chunk.js"
+  },
+  {
+    "revision": "25e460493ae0a359e282",
+    "url": "./static/js/runtime-main.bdae9d4b.js"
+  }
+]);
---
+++ jobzilla/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Web site under construction.</p>
+        <h2 className="App-link">Stay tunned!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
